6873f135b2b7a3f5b5a44220e5824d4e823c6704
ZinatAfrooz <S01012776@acad.tri-c.edu>
2018-02-13 01:24:41 -0500

    Lab04 Room Charge App

---
+++ RoomChargeApp/RoomChargeApp/Form1.Designer.vb
@@ -0,0 +1,423 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.txtNightCharge = New System.Windows.Forms.TextBox()
+        Me.txtNightNum = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.txtMisc = New System.Windows.Forms.TextBox()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.txtTelephone = New System.Windows.Forms.TextBox()
+        Me.txtRoomService = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btn_Exit = New System.Windows.Forms.Button()
+        Me.Panel3 = New System.Windows.Forms.Panel()
+        Me.lblTotCharge = New System.Windows.Forms.Label()
+        Me.lblTax = New System.Windows.Forms.Label()
+        Me.lblSubTotal = New System.Windows.Forms.Label()
+        Me.lblAddCharge = New System.Windows.Forms.Label()
+        Me.lblRoomCharge = New System.Windows.Forms.Label()
+        Me.Label15 = New System.Windows.Forms.Label()
+        Me.Label14 = New System.Windows.Forms.Label()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.lblTodaysDate = New System.Windows.Forms.Label()
+        Me.lblTime = New System.Windows.Forms.Label()
+        Me.Panel1.SuspendLayout()
+        Me.Panel2.SuspendLayout()
+        Me.Panel3.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(189, 13)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(249, 24)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Room Charge Application"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(35, 60)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(73, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Today's Date:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(35, 89)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(33, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Time:"
+        '
+        'Panel1
+        '
+        Me.Panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel1.Controls.Add(Me.txtNightCharge)
+        Me.Panel1.Controls.Add(Me.txtNightNum)
+        Me.Panel1.Controls.Add(Me.Label6)
+        Me.Panel1.Controls.Add(Me.Label5)
+        Me.Panel1.Location = New System.Drawing.Point(38, 147)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(264, 113)
+        Me.Panel1.TabIndex = 3
+        '
+        'txtNightCharge
+        '
+        Me.txtNightCharge.Location = New System.Drawing.Point(131, 47)
+        Me.txtNightCharge.Name = "txtNightCharge"
+        Me.txtNightCharge.Size = New System.Drawing.Size(100, 20)
+        Me.txtNightCharge.TabIndex = 8
+        '
+        'txtNightNum
+        '
+        Me.txtNightNum.Location = New System.Drawing.Point(131, 10)
+        Me.txtNightNum.Name = "txtNightNum"
+        Me.txtNightNum.Size = New System.Drawing.Size(100, 20)
+        Me.txtNightNum.TabIndex = 7
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(3, 47)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(79, 13)
+        Me.Label6.TabIndex = 6
+        Me.Label6.Text = "Nightly Charge:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(3, 10)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(40, 13)
+        Me.Label5.TabIndex = 5
+        Me.Label5.Text = "Nights:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(35, 131)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(56, 13)
+        Me.Label4.TabIndex = 4
+        Me.Label4.Text = "Room Info"
+        '
+        'Panel2
+        '
+        Me.Panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel2.Controls.Add(Me.txtMisc)
+        Me.Panel2.Controls.Add(Me.Label9)
+        Me.Panel2.Controls.Add(Me.txtTelephone)
+        Me.Panel2.Controls.Add(Me.txtRoomService)
+        Me.Panel2.Controls.Add(Me.Label7)
+        Me.Panel2.Controls.Add(Me.Label8)
+        Me.Panel2.Location = New System.Drawing.Point(317, 147)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(264, 113)
+        Me.Panel2.TabIndex = 9
+        '
+        'txtMisc
+        '
+        Me.txtMisc.Location = New System.Drawing.Point(131, 77)
+        Me.txtMisc.Name = "txtMisc"
+        Me.txtMisc.Size = New System.Drawing.Size(100, 20)
+        Me.txtMisc.TabIndex = 10
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(3, 77)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(32, 13)
+        Me.Label9.TabIndex = 9
+        Me.Label9.Text = "Misc:"
+        '
+        'txtTelephone
+        '
+        Me.txtTelephone.Location = New System.Drawing.Point(131, 47)
+        Me.txtTelephone.Name = "txtTelephone"
+        Me.txtTelephone.Size = New System.Drawing.Size(100, 20)
+        Me.txtTelephone.TabIndex = 8
+        '
+        'txtRoomService
+        '
+        Me.txtRoomService.Location = New System.Drawing.Point(131, 10)
+        Me.txtRoomService.Name = "txtRoomService"
+        Me.txtRoomService.Size = New System.Drawing.Size(100, 20)
+        Me.txtRoomService.TabIndex = 7
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(3, 47)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(61, 13)
+        Me.Label7.TabIndex = 6
+        Me.Label7.Text = "Telephone:"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(3, 10)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(77, 13)
+        Me.Label8.TabIndex = 5
+        Me.Label8.Text = "Room Service:"
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Location = New System.Drawing.Point(76, 464)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(75, 23)
+        Me.btnCalculate.TabIndex = 10
+        Me.btnCalculate.Text = "Calculate"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(227, 464)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(75, 23)
+        Me.btnClear.TabIndex = 11
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btn_Exit
+        '
+        Me.btn_Exit.Location = New System.Drawing.Point(381, 464)
+        Me.btn_Exit.Name = "btn_Exit"
+        Me.btn_Exit.Size = New System.Drawing.Size(75, 23)
+        Me.btn_Exit.TabIndex = 12
+        Me.btn_Exit.Text = "Exit"
+        Me.btn_Exit.UseVisualStyleBackColor = True
+        '
+        'Panel3
+        '
+        Me.Panel3.Controls.Add(Me.lblTotCharge)
+        Me.Panel3.Controls.Add(Me.lblTax)
+        Me.Panel3.Controls.Add(Me.lblSubTotal)
+        Me.Panel3.Controls.Add(Me.lblAddCharge)
+        Me.Panel3.Controls.Add(Me.lblRoomCharge)
+        Me.Panel3.Controls.Add(Me.Label15)
+        Me.Panel3.Controls.Add(Me.Label14)
+        Me.Panel3.Controls.Add(Me.Label13)
+        Me.Panel3.Controls.Add(Me.Label12)
+        Me.Panel3.Controls.Add(Me.Label11)
+        Me.Panel3.Location = New System.Drawing.Point(67, 280)
+        Me.Panel3.Name = "Panel3"
+        Me.Panel3.Size = New System.Drawing.Size(434, 167)
+        Me.Panel3.TabIndex = 13
+        '
+        'lblTotCharge
+        '
+        Me.lblTotCharge.AutoSize = True
+        Me.lblTotCharge.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblTotCharge.Location = New System.Drawing.Point(171, 125)
+        Me.lblTotCharge.Name = "lblTotCharge"
+        Me.lblTotCharge.Padding = New System.Windows.Forms.Padding(3, 2, 100, 2)
+        Me.lblTotCharge.Size = New System.Drawing.Size(105, 19)
+        Me.lblTotCharge.TabIndex = 9
+        '
+        'lblTax
+        '
+        Me.lblTax.AutoSize = True
+        Me.lblTax.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblTax.Location = New System.Drawing.Point(171, 95)
+        Me.lblTax.Name = "lblTax"
+        Me.lblTax.Padding = New System.Windows.Forms.Padding(3, 2, 100, 2)
+        Me.lblTax.Size = New System.Drawing.Size(105, 19)
+        Me.lblTax.TabIndex = 8
+        '
+        'lblSubTotal
+        '
+        Me.lblSubTotal.AutoSize = True
+        Me.lblSubTotal.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblSubTotal.Location = New System.Drawing.Point(171, 67)
+        Me.lblSubTotal.Name = "lblSubTotal"
+        Me.lblSubTotal.Padding = New System.Windows.Forms.Padding(3, 2, 100, 2)
+        Me.lblSubTotal.Size = New System.Drawing.Size(105, 19)
+        Me.lblSubTotal.TabIndex = 7
+        '
+        'lblAddCharge
+        '
+        Me.lblAddCharge.AutoSize = True
+        Me.lblAddCharge.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblAddCharge.Location = New System.Drawing.Point(171, 41)
+        Me.lblAddCharge.Name = "lblAddCharge"
+        Me.lblAddCharge.Padding = New System.Windows.Forms.Padding(3, 2, 100, 2)
+        Me.lblAddCharge.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.lblAddCharge.Size = New System.Drawing.Size(105, 19)
+        Me.lblAddCharge.TabIndex = 6
+        '
+        'lblRoomCharge
+        '
+        Me.lblRoomCharge.AutoSize = True
+        Me.lblRoomCharge.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblRoomCharge.Location = New System.Drawing.Point(171, 13)
+        Me.lblRoomCharge.Name = "lblRoomCharge"
+        Me.lblRoomCharge.Padding = New System.Windows.Forms.Padding(3, 2, 100, 2)
+        Me.lblRoomCharge.Size = New System.Drawing.Size(105, 19)
+        Me.lblRoomCharge.TabIndex = 5
+        '
+        'Label15
+        '
+        Me.Label15.AutoSize = True
+        Me.Label15.Location = New System.Drawing.Point(5, 125)
+        Me.Label15.Name = "Label15"
+        Me.Label15.Size = New System.Drawing.Size(76, 13)
+        Me.Label15.TabIndex = 4
+        Me.Label15.Text = "Total Charges:"
+        '
+        'Label14
+        '
+        Me.Label14.AutoSize = True
+        Me.Label14.Location = New System.Drawing.Point(5, 95)
+        Me.Label14.Name = "Label14"
+        Me.Label14.Size = New System.Drawing.Size(25, 13)
+        Me.Label14.TabIndex = 3
+        Me.Label14.Text = "Tax"
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Location = New System.Drawing.Point(5, 67)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(49, 13)
+        Me.Label13.TabIndex = 2
+        Me.Label13.Text = "Subtotal:"
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Location = New System.Drawing.Point(3, 41)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(98, 13)
+        Me.Label12.TabIndex = 1
+        Me.Label12.Text = "Additional Charges:"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(4, 13)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(80, 13)
+        Me.Label11.TabIndex = 0
+        Me.Label11.Text = "Room Charges:"
+        '
+        'lblTodaysDate
+        '
+        Me.lblTodaysDate.AutoSize = True
+        Me.lblTodaysDate.Location = New System.Drawing.Point(183, 60)
+        Me.lblTodaysDate.Name = "lblTodaysDate"
+        Me.lblTodaysDate.Size = New System.Drawing.Size(0, 13)
+        Me.lblTodaysDate.TabIndex = 14
+        '
+        'lblTime
+        '
+        Me.lblTime.AutoSize = True
+        Me.lblTime.Location = New System.Drawing.Point(183, 89)
+        Me.lblTime.Name = "lblTime"
+        Me.lblTime.Size = New System.Drawing.Size(0, 13)
+        Me.lblTime.TabIndex = 15
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(602, 499)
+        Me.Controls.Add(Me.lblTime)
+        Me.Controls.Add(Me.lblTodaysDate)
+        Me.Controls.Add(Me.Panel3)
+        Me.Controls.Add(Me.btn_Exit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Room Charge App"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel2.PerformLayout()
+        Me.Panel3.ResumeLayout(False)
+        Me.Panel3.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents txtNightCharge As TextBox
+    Friend WithEvents txtNightNum As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents txtMisc As TextBox
+    Friend WithEvents Label9 As Label
+    Friend WithEvents txtTelephone As TextBox
+    Friend WithEvents txtRoomService As TextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents btnCalculate As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btn_Exit As Button
+    Friend WithEvents Panel3 As Panel
+    Friend WithEvents lblTotCharge As Label
+    Friend WithEvents lblTax As Label
+    Friend WithEvents lblSubTotal As Label
+    Friend WithEvents lblAddCharge As Label
+    Friend WithEvents lblRoomCharge As Label
+    Friend WithEvents Label15 As Label
+    Friend WithEvents Label14 As Label
+    Friend WithEvents Label13 As Label
+    Friend WithEvents Label12 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents lblTodaysDate As Label
+    Friend WithEvents lblTime As Label
+End Class
---
+++ RoomChargeApp/RoomChargeApp/Form1.vb
@@ -0,0 +1,51 @@
+﻿Public Class Form1
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        lblTodaysDate.Text = Now.ToString("D")
+        lblTime.Text = Now.ToString("T")
+    End Sub
+
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        ' Declare variables for the calculations.
+        Dim decRoomCharges As Decimal ' Room charges total
+        Dim decAddCharges As Decimal ' Additional charges
+        Dim decSubtotal As Decimal ' Subtotal
+        Dim decTax As Decimal ' Tax
+        Dim decTotal As Decimal ' Total of all charges
+        Dim intNumofNight As Int32
+        Dim decNightlyCharge As Decimal
+        Const decTAX_RATE As Decimal = 0.08D ' Tax rate
+
+        Try
+            intNumofNight = Convert.ToInt16(txtNightNum.Text)
+            decNightlyCharge = CDec(txtNightCharge.Text)
+            decRoomCharges = CDec(intNumofNight * decNightlyCharge)
+            decAddCharges = CDec(txtRoomService.Text) + CDec(txtTelephone.Text) + CDec(txtMisc.Text)
+            decSubtotal = decRoomCharges + decAddCharges
+            decTax = decTAX_RATE * decSubtotal
+            decTotal = decSubtotal + decTax
+
+            lblRoomCharge.Text = decRoomCharges.ToString("C")
+            lblAddCharge.Text = decAddCharges.ToString("C")
+            lblSubTotal.Text = decSubtotal.ToString("C")
+            lblTotCharge.Text = decTotal.ToString("C")
+            lblTax.Text = decTax.ToString("C")
+        Catch
+            ' Error message
+            MessageBox.Show("All input must be valid numeric values.")
+        End Try
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        txtNightNum.Clear()
+        txtNightCharge.Clear()
+        txtMisc.Clear()
+        txtRoomService.Clear()
+        txtTelephone.Clear()
+    End Sub
+
+    Private Sub btn_Exit_Click(sender As Object, e As EventArgs) Handles btn_Exit.Click
+        Application.Exit()
+    End Sub
+End Class
